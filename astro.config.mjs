371bed357eb770fbafcf6d9b26057e0a60febb1f
Nerando Johnson <nerajno@gmail.com>
2025-09-15 07:15:51 -0600

    adding clarity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import clarity from '@kbyte-tech/astro-clarity';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://your-domain.com', // Update with your actual domain
+  integrations: [
+    tailwind(),
+     clarity({
+      enabled: true,
+      projectId: 'surxnzd38u',
+    }),
+  ],
+  site: 'learnt.developingdvlpr.com', // Update with your actual domain
   build: {
     inlineStylesheets: 'auto',
   },
